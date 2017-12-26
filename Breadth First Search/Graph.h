792135cae8bba4f78c64c64e6bca5f52ebe22c90
rlingaiah <33558734+rlingaiah@users.noreply.github.com>
2017-12-26 15:29:06 -0800

    Update Graph.h

--- Breadth First Search/Graph.h
+++ Breadth First Search/Graph.h
@@ -1,12 +1,12 @@
 //Rhea Lingaiah
 //rlingaia
-//PA5
+//PA4
 #ifndef _GRAPH_H_INCLUDE_
 #define _GRAPH_H_INCLUDE_
 #include <stdio.h>
 #include <stdlib.h>
 #include "List.h"
-#define UNDEF -1
+#define INF -1
 #define NIL 0
 
 
@@ -18,14 +18,15 @@ Graph newGraph(int n);
 void freeGraph(Graph* pG);
 int getOrder(Graph G);
 int getSize(Graph G);
+int getSource(Graph G);
 int getParent(Graph G, int u);
-int getDiscover(Graph G, int u);
-int getFinish(Graph G, int u);
+int getDist(Graph G, int u);
+void getPath(List L, Graph G, int u);
+
+void makeNull(Graph G);
 void addEdge(Graph G, int u, int v);
 void addArc(Graph G, int u, int v);
-void DFS(Graph G, List s);
-Graph transpose(Graph G);
-Graph copyGraph(Graph G);
+void BFS(Graph G, int s);
 void printGraph(FILE* out, Graph G);
 
 #endif
